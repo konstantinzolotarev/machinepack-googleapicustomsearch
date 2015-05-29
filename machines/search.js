6562b345f715759225a20f24e55609f917b6cf46
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-05-29 14:13:35 -0700

    updated pack

--- machines/search.js
+++ machines/search.js
@@ -1,3 +1,9 @@
+'use strict';
+
+var _ = require('lodash');
+var googleapis = require('googleapis');
+var customsearch = googleapis.customsearch('v1');
+
 module.exports = {
 
 
@@ -7,7 +13,7 @@ module.exports = {
   description: 'Executes the search using Customsearch API call',
 
 
-  cacheable: false,
+  cacheable: true,
 
 
   sync: false,
@@ -17,25 +23,63 @@ module.exports = {
 
 
   inputs: {
-
+    q: {
+      example: 'test search',
+      description: 'Search phrase',
+      required: true
+    },
+    auth: {
+      example: 'AIzaSyAYVDlaoVs_GZw9JNvSclRWH_PEMKII6tc',
+      description: 'You generated API_KEY'
+    },
+    alt: {
+      example: 'atom',
+      description: 'JSON/Atom Custom Search API can return results in one of two formats. JSON is the default data format; you can get results in Atom format by specifying the alt=atom query parameter'
+    },
+    c2coff: {
+      example: '',
+      description: 'Turns off the translation between zh-CN and zh-TW.'
+    },
+    cr: {
+      example: '',
+      description: 'Country restrict(s)'
+    },
+    fileType: {
+      example: 'jpg',
+      description: 'Returns images of a specified type. Some of the allowed values are: bmp, gif, png, jpg, svg, pdf, ...'
+    },
+    searchType: {
+      example: 'image',
+      description: 'Specifies the search type'
+    },
+    sort: {
+      example: '',
+      description: 'The sort expression to apply to the results'
+    },
+    start: {
+      example: 15,
+      description: 'The index of the first result to return'
+    }
   },
-
 
   exits: {
 
     success: {
       variableName: 'result',
-      description: 'Done.',
-    },
+      description: 'Search results.',
+    }
 
   },
 
-
-  fn: function (inputs,exits
-  /**/) {
-    return exits.success();
-  },
-
-
+  fn: function(inputs, exits) {
+    var params = {};
+    _.merge(params, inputs);
+    customsearch.cse.list(params, function(err, result) {
+      if (err) {
+        return exits.error(err);
+      }
+      return exits.success(result);
+    });
+  }
 
 };
